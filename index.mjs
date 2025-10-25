68d4cc4166651e7b779cbb79655f7776ea2bff5f
Peg Leg <20299235+peg-leg@users.noreply.github.com>
2025-10-25 13:32:03 +0200

    Delete index.mjs

--- index.mjs
+++
@@ -1,5 +0,0 @@
-import { readFileSync } from 'fs';
-
-export const wordles = JSON.parse(readFileSync('wordles.json'));
-export default wordles;
-export const nonwordles = JSON.parse(readFileSync('nonwordles.json'));
